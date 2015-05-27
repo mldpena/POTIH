f9de9c82684083f5e2d4e749f9028ba1aa51c2f2
mldpena <marklawrencepena@gmail.com>
2015-05-27 19:48:18 +0800

    Fixed product controller

---
+++ application/controllers/product.php
@@ -0,0 +1,178 @@
+@@ -1,177 +0,0 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Product extends CI_Controller {
+	
+	/**
+	 * Load needed model or library for the current controller
+	 * @return [none]
+	 */
+	
+	private function _load_libraries()
+	{
+		$this->load->model('product_model');
+		$this->load->helper('authentication');
+		$this->load->helper('query');
+	}
+
+	/**
+	 * Default method for the controller
+	 * @return [none]
+	 */
+	
+	public function index()
+	{	
+		$this->_load_libraries();
+
+		check_user_credentials();
+
+		$page = $this->uri->segment(2);
+
+		if (isset($_POST['data'])) 
+		{
+			$this->_ajax_request();
+			exit();
+		}
+
+		switch ($page) 
+		{
+			case 'list':
+				$page = 'product_list';
+				/* Temporary */
+				$data['branch_list'] 	= get_name_list_from_table(TRUE,'branch',TRUE);
+				$data['material_list'] 	= get_name_list_from_table(TRUE,'material_type',TRUE);
+				$data['subgroup_list'] 	= get_name_list_from_table(TRUE,'subgroup',TRUE);
+				break;
+			
+			default:
+
+				break;
+		}
+
+		$data['name']	= get_user_fullname();
+		$data['branch']	= get_branch_name();
+		$data['token']	= '&'.$this->security->get_csrf_token_name().'='.$this->security->get_csrf_hash();
+		$data['page'] 	= $page;
+		$data['script'] = $page.'_js.php';
+
+		$this->load->view('master', $data);
+	}
+
+	/**
+	 * Forces controller to always go to index instead of directly accessing the methods
+	 * @return [none]
+	 */
+	
+	public function _remap()
+	{
+        $param_offset = 1;
+        $method = 'index';
+	    $params = array_slice($this->uri->rsegment_array(), $param_offset);
+
+	    call_user_func_array(array($this, $method), $params);
+	} 
+
+	/**
+	 * List of AJAX request
+	 * @return [none]
+	 */
+	
+	private function _ajax_request()
+	{
+		$post_data 	= array();
+		$fnc 		= '';
+
+		$post_data 	= xss_clean(json_decode($this->input->post('data'),true));
+		$fnc 		= $post_data['fnc'];
+
+		switch ($fnc) 
+		{
+			case 'get_product_list':
+				$this->_get_product_list($post_data);
+				break;
+
+			case 'get_material_and_subgroup':
+				$this->_get_product_material_and_subgroup($post_data);
+				break;
+
+			case 'insert_new_product':
+				$this->_insert_new_product($post_data);
+				break;
+
+			case 'get_product_details':
+				$this->_get_product_details($post_data);
+				break;
+
+			case 'update_product_details':
+				$this->_update_product_details($post_data);
+				break;
+
+			case 'delete_product':
+				$this->_delete_product($post_data);
+				break;
+
+			case 'get_branch_list_for_min_max':
+				$this->_get_branch_list();
+				break;
+
+			default:
+				
+				break;
+		}
+
+	}
+
+	private function _get_product_list($param)
+	{
+		$response = $this->product_model->get_product_list($param);
+		echo json_encode($response);
+	}
+
+	private function _get_product_material_and_subgroup($param)
+	{
+		$response = $this->product_model->get_product_material_subgroup($param);
+		echo json_encode($response);
+	}
+
+	private function _insert_new_product($param)
+	{
+		$response = $this->product_model->insert_new_product($param);
+		echo json_encode($response);
+	}
+
+	private function _get_product_details($param)
+	{
+		$response = $this->product_model->get_product_details($param);
+		echo json_encode($response);
+	}
+
+	private function _update_product_details($param)
+	{
+		$response = $this->product_model->update_product_details($param);
+		echo json_encode($response);
+	}
+
+	private function _delete_product($param)
+	{
+		$response = $this->product_model->delete_product($param);
+		echo json_encode($response);
+	}
+
+	private function _get_branch_list()
+	{
+		$i = 0;
+		$response = array();
+		$branch_list = get_name_list_from_table(FALSE,'branch');
+	
+		foreach ($branch_list as $key => $value) {
+			$response['data'][$i][] = array(0);		
+			$response['data'][$i][] = array($i+1);		
+			$response['data'][$i][] = array($value,$key);		
+			$response['data'][$i][] = array(1);		
+			$response['data'][$i][] = array(1);	
+			$i++;		
+		}
+
+		echo json_encode($response);
+	}
+}
